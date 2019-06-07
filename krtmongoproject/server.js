543f6a79e85e687059a7dfcf7c591e0aa6e0f09b
Khavya Seshadri <ks4049@rit.edu>
2019-06-06 21:33:19 -0400

    port change

--- krtmongoproject/server.js
+++ krtmongoproject/server.js
@@ -1,7 +1,6 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 3000;
 const express = require('express')
 const bodyParser= require('body-parser')
 const app = express();
@@ -114,5 +113,5 @@ app.get('/getRestaurantsByLocation',function(req, res){
 	});
 });
 
-app.listen(port, hostname);
+app.listen(port, () => console.log(`Listening on port: ${port}`));
 console.log('Search based application');
